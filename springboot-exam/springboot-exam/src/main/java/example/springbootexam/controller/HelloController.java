440e4acc9b8847b9925532697e107f0e9c7dec1a
mini0shark <mini0shark@naver.com>
2021-06-22 01:07:34 +0900

    feat: Spring입문 (스프링부트) 인프런강의
    스프링 기초
    정적 컨텐츠 , MVC와 템플릿 엔진, API 방식 별 Controller 실습
    
    #Resolved 20210622

--- springboot-exam/springboot-exam/src/main/java/example/springbootexam/controller/HelloController.java
+++ springboot-exam/springboot-exam/src/main/java/example/springbootexam/controller/HelloController.java
@@ -3,6 +3,8 @@ package example.springbootexam.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -12,9 +14,37 @@ public class HelloController {
         return "hello";
     }
 
-    @GetMapping("second")
-    public String second(Model model){
-        model.addAttribute("data", "Spring!!");
-        return "second";
+    // mvc-방식
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam(value = "name") String name, Model model){
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
+
+
+    @GetMapping("hello-string")
+    @ResponseBody   // requestBody에 리턴값을 그대로 넣어주겠다는 것
+    public String helloString(@RequestParam(value = "name") String name){
+        return "hello "+name;
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name){
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+    static class Hello{
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
